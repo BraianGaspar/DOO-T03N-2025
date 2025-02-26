3760338175f3db97ec9ef0c10aae70f963ee29ff
Braian Gaspar <braian_gaspar@hotmail.com>
2025-02-25 22:17:20 -0300

    Criando método Holaqueta
    Método hola que tal.

---
+++ Alunos/Braian/Aula02/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        helloWorld();
+
+    }
+
+    public static void helloWorld(){
+        System.out.println("Hello, World!");
+    }
+    public static void OlaMundol(){
+        System.out.println("Ola Mundo");
+    }
+    public static void HolaMundo(){
+        System.out.println("Hola Mundo");
+    }
+}
