13f297a0f857cf2c125cd4c690bd15c5a4c59d0f
Braian Gaspar <braian_gaspar@hotmail.com>
2025-02-25 22:16:04 -0300

    Criando merge nova
    Merge nova

---
+++ Alunos/Braian/Aula02/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        helloWorld();
+
+    }
+
+    public static void helloWorld(){
+        System.out.println("Hello, World!");
+    }
+    public static void OlaMundol(){
+        System.out.println("Ola Mundo");
+    }
+}
